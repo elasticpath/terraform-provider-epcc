36777ed0954ee679c31c50126293491ed1fd5aea
Alex Razkevich <razkevich8@gmail.com>
2021-05-13 21:54:30 +0300

    Implement Currencies [/v2/currencies] - account acc test

--- internal/provider/data_source_epcc_currency.go
+++ internal/provider/data_source_epcc_currency.go
@@ -10,18 +10,6 @@ import (
 func dataSourceEpccCurrency() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceEpccCurrencyRead,
-		Schema: map[string]*schema.Schema{
-			"id":                 {Type: schema.TypeString, Required: true},
-			"type":               {Type: schema.TypeString, Computed: true},
-			"code":               {Type: schema.TypeString, Computed: true},
-			"exchange_rate":      {Type: schema.TypeInt, Computed: true},
-			"format":             {Type: schema.TypeString, Computed: true},
-			"decimal_point":      {Type: schema.TypeString, Computed: true},
-			"thousand_separator": {Type: schema.TypeString, Computed: true},
-			"decimal_places":     {Type: schema.TypeInt, Computed: true},
-			"default":            {Type: schema.TypeBool, Computed: true},
-			"enabled":            {Type: schema.TypeBool, Computed: true},
-		},
 	}
 }
 
@@ -33,16 +21,6 @@ func dataSourceEpccCurrencyRead(ctx context.Context, d *schema.ResourceData, m i
 	if err != nil {
 		return FromAPIError(err)
 	}
-
-	d.Set("type", currency.Data.Type)
-	d.Set("code", currency.Data.Code)
-	d.Set("exchange_rate", currency.Data.ExchangeRate)
-	d.Set("format", currency.Data.Format)
-	d.Set("decimal_point", currency.Data.DecimalPoint)
-	d.Set("thousand_separator", currency.Data.ThousandSeparator)
-	d.Set("decimal_places", currency.Data.DecimalPlaces)
-	d.Set("default", currency.Data.Default)
-	d.Set("enabled", currency.Data.Enabled)
 
 	d.SetId(currency.Data.Id)
 
