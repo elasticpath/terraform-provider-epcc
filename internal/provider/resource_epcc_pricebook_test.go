5a9e671e513cec50b5790b5a25ee1b1c04ab5a34
David MacBride <david.macbride@elasticpath.com>
2021-05-13 13:56:27 -0400

    Initial pricebook acceptance test

---
+++ internal/provider/resource_epcc_pricebook_test.go
@@ -0,0 +1,31 @@
+package provider
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourcePricebook(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourcePricebook,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr("epcc_pricebook.acc_test_epcc_pricebook", "name", regexp.MustCompile("Pricebook for acc tests name")),
+					resource.TestMatchResourceAttr("epcc_pricebook.acc_test_epcc_pricebook", "description", regexp.MustCompile("Pricebook for acc tests description")),
+				),
+			},
+		},
+	})
+}
+
+const testAccResourcePricebook = `
+resource "epcc_pricebook" "acc_test_epcc_pricebook" {
+  name            = "Pricebook for acc tests name"
+  description      = "Pricebook for acc tests description"
+}
+`
