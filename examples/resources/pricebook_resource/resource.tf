3d3ca530c0a1309c0dd8f36a9a9601a057150000
David MacBride <david.macbride@elasticpath.com>
2021-05-13 00:55:14 -0400

    Rebase

---
+++ examples/resources/pricebook_resource/resource.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    epcc = {
+      version = "0.0.1"
+      source  = "elasticpath.com/elasticpath/epcc"
+    }
+  }
+}
+
+resource "epcc_pricebook" "my_first_terraform_pricebook" {
+  name  = "TFPricebook1"
+  description = "Terraform 1"
+}
+
+resource "epcc_pricebook" "my_second_terraform_pricebook" {
+  name  = "TFPricebook2"
+  description = "Terraform 2"
+}
