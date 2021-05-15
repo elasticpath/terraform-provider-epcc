a2b1a045e7669b6e13d220616da318cf0b9b983f
Steve Ramage <49958178+steve-r-west@users.noreply.github.com>
2021-05-15 11:38:51 -0700

    Resolves #82 - Add Terraform Format Check to CI (#83)

--- examples/resources/catalog_resource/resource.tf
+++ examples/resources/catalog_resource/resource.tf
@@ -27,7 +27,7 @@ resource "epcc_node" "my_first_node" {
   description  = "Node"
   slug         = "node-3"
   hierarchy_id = epcc_hierarchy.my_first_hierarchy.id
-  products = [ epcc_product.my_first_terraform_physical_product.id ]
+  products     = [epcc_product.my_first_terraform_physical_product.id]
 }
 
 resource "epcc_pricebook" "my_first_terraform_pricebook" {
@@ -36,8 +36,8 @@ resource "epcc_pricebook" "my_first_terraform_pricebook" {
 }
 
 resource "epcc_catalog" "my_first_catalog" {
-  name = "My Second Catalog"
+  name        = "My Second Catalog"
   description = "Catalog created  II by Terraform"
-  hierarchies = [ epcc_hierarchy.my_first_hierarchy.id]
-  pricebook = epcc_pricebook.my_first_terraform_pricebook.id
+  hierarchies = [epcc_hierarchy.my_first_hierarchy.id]
+  pricebook   = epcc_pricebook.my_first_terraform_pricebook.id
 }
