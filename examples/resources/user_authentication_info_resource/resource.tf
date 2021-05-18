2c52105971cef141729acdf6424b249c930cd7d2
Alex Razkevich <razkevich8@gmail.com>
2021-05-18 14:51:38 +0300

    test

--- examples/resources/user_authentication_info_resource/resource.tf
+++ examples/resources/user_authentication_info_resource/resource.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     epcc = {
       version = "0.0.1"
-      source = "elasticpath.com/elasticpath/epcc"
+      source  = "elasticpath.com/elasticpath/epcc"
     }
   }
 }
@@ -17,12 +17,12 @@ resource "epcc_realm" "test_realm_for_user_authentication_info" {
     "https://google.com/"
   ]
   duplicate_email_policy = "allowed"
-  origin_id = "hello-world"
-  origin_type = "customer-authentication-settings"
+  origin_id              = "hello-world"
+  origin_type            = "customer-authentication-settings"
 }
 
 resource "epcc_user_authentication_info" "test_user_authentication_info" {
-  name = "John Doe 2"
-  email = "john.doe@banks.com"
+  name     = "John Doe 2"
+  email    = "john.doe@banks.com"
   realm_id = epcc_realm.test_realm_for_user_authentication_info.id
 }
