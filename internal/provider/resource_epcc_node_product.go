b1efe01b107cc8bd73b65d66ae11dad1f589745a
Steve Ramage <steve.ramage@elasticpath.com>
2021-06-03 20:13:15 -0700

    Resolves #123 - Add mutex for node product relationships

--- internal/provider/resource_epcc_node_product.go
+++ internal/provider/resource_epcc_node_product.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/elasticpath/terraform-provider-epcc/external/sdk/epcc"
+	"github.com/elasticpath/terraform-provider-epcc/internal/provider/util"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
+
+var mutex = util.NewMutexKV()
 
 func resourceEpccNodeProduct() *schema.Resource {
 	return &schema.Resource{
@@ -57,7 +60,11 @@ func resourceEpccNodeProductDelete(ctx context.Context, d *schema.ResourceData, 
 		Data: &list,
 	}
 
+	// PCM seems to have issues if we concurrently call endpoint
+	// Even if we try to lock based on node id
+	mutex.Lock("nodes")
 	err := epcc.Nodes.DeleteNodeProduct(&ctx, client, hierarchyId, nodeId, wrappedList)
+	mutex.Unlock("nodes")
 
 	if err != nil {
 		ReportAPIError(ctx, err)
@@ -73,7 +80,9 @@ func resourceEpccNodeProductRead(ctx context.Context, d *schema.ResourceData, m 
 	nodeId := d.Get("node_id").(string)
 	productId := d.Get("product_id").(string)
 
+	mutex.Lock("nodes")
 	nodeProduct, err := epcc.Nodes.GetNodeProducts(&ctx, client, hierarchyId, nodeId)
+	mutex.Unlock("nodes")
 
 	if err != nil {
 		ReportAPIError(ctx, err)
@@ -129,9 +138,11 @@ func resourceEpccNodeProductCreate(ctx context.Context, d *schema.ResourceData, 
 
 	relationships := convertIdsToTypeIdRelationship("product", []string{productId})
 
+	mutex.Lock("nodes")
 	apiError := epcc.Nodes.CreateNodeProducts(&ctx, client, hierarchyId, nodeId, epcc.DataForTypeIdRelationshipList{
 		Data: &relationships,
 	})
+	mutex.Unlock("nodes")
 
 	if apiError != nil {
 		ReportAPIError(ctx, apiError)
