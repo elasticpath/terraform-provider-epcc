8c3360d706cb43ba72dec9afd63f159b183c434b
Steve Ramage <steve.ramage@elasticpath.com>
2021-06-03 20:33:51 -0700

    Code Review Feedback

--- external/sdk/epcc/settings.go
+++ external/sdk/epcc/settings.go
@@ -36,59 +36,6 @@ func (settings) Get(ctx *context.Context, client *Client) (*SettingsData, ApiErr
 	return &settings, nil
 }
 
-//// GetAll fetches all profiles
-//func (profiles) GetAll(ctx *context.Context, client *Client, realmId string) (*ProfileList, ApiErrors) {
-//	path := fmt.Sprintf("/v2/authentication-realms/%s/oidc-profiles", realmId)
-//
-//	body, apiError := client.DoRequest(ctx, "GET", path, "", nil)
-//	if apiError != nil {
-//		return nil, apiError
-//	}
-//
-//	var profiles ProfileList
-//	if err := json.Unmarshal(body, &profiles); err != nil {
-//		return nil, FromError(err)
-//	}
-//
-//	return &profiles, nil
-//}
-////
-////// Create creates a profile
-////func (profiles) Create(ctx *context.Context, client *Client, profile *Profile) (*ProfileData, ApiErrors) {
-////	profileData := ProfileData{
-////		Data: *profile,
-////	}
-////
-////	jsonPayload, err := json.Marshal(profileData)
-////	if err != nil {
-////		return nil, FromError(err)
-////	}
-////
-////	path := fmt.Sprintf("/v2/authentication-realms/%s/oidc-profiles/", profile.RealmId)
-////
-////	body, apiError := client.DoRequest(ctx, "POST", path, "", bytes.NewBuffer(jsonPayload))
-////	if apiError != nil {
-////		return nil, apiError
-////	}
-////	var newProfile ProfileData
-////	if err := json.Unmarshal(body, &newProfile); err != nil {
-////		return nil, FromError(err)
-////	}
-////
-////	return &newProfile, nil
-////}
-//
-//// Delete deletes a profile.
-//func (profiles) Delete(ctx *context.Context, client *Client, profileID, realmId string) ApiErrors {
-//	path := fmt.Sprintf("/v2/authentication-realms/%s/oidc-profiles/%s", realmId, profileID)
-//
-//	if _, err := client.DoRequest(ctx, "DELETE", path, "", nil); err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-
 // Update updates a profile.
 func (settings) Update(ctx *context.Context, client *Client, settings Settings) (*SettingsData, ApiErrors) {
 
