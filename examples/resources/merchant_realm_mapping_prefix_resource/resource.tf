15699ee613e8bb601a932ce48bdd9841b554a5f9
razkevich <razkevich8@gmail.com>
2021-06-07 18:44:01 +0300

    Merchant authentication settings (#128)
    * merchant realm mappings
    
    * merchant realm mappings
    
    * merchant realm mappings
    
    * merchant realm mappings
    
    * merchant realm mappings
    
    * merchant realm mappings

---
+++ examples/resources/merchant_realm_mapping_prefix_resource/resource.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    epcc = {
+      version = "0.0.1"
+      source  = "elasticpath.com/elasticpath/epcc"
+    }
+  }
+}
+
+provider "epcc" {
+}
+
+data "epcc_merchant_realm_mappings" "test_merchant_realm_mappings" {
+}
+
+resource "epcc_merchant_realm_mapping_prefix" "test_merchant_realm_mapping_prefix" {
+  merchant_realm_mapping_id = data.epcc_merchant_realm_mappings.test_merchant_realm_mappings.merchant_realm_mapping_id
+  prefix                    = "abcdefgh"
+}
---
+++ internal/provider/resource_epcc_merchant_realm_mapping_prefix_test.go
@@ -0,0 +1,46 @@
+package provider
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceMerchantRealmMappingPrefix(t *testing.T) {
+	myRandSeq := randSeq(10)
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccResourceMerchantRealmMappingPrefix, myRandSeq),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("epcc_merchant_realm_mapping_prefix.test_merchant_realm_mapping_prefix", "prefix", myRandSeq),
+				),
+			},
+		},
+	})
+}
+
+// language=HCL
+const testAccResourceMerchantRealmMappingPrefix = `
+data "epcc_merchant_realm_mappings" "test_merchant_realm_mappings" {
+}
+resource "epcc_merchant_realm_mapping_prefix" "test_merchant_realm_mapping_prefix" {
+  merchant_realm_mapping_id = data.epcc_merchant_realm_mappings.test_merchant_realm_mappings.merchant_realm_mapping_id
+  prefix = "%s"
+}
+`
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+func randSeq(n int) string {
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
