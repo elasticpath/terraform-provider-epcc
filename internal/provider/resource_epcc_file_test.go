e4fdab336bf634552421848186ef953f3f60b5e5
Steve Ramage <steve.ramage@elasticpath.com>
2022-02-04 14:30:02 -0800

    Fixed Tests hopefully

--- internal/provider/resource_epcc_file_test.go
+++ internal/provider/resource_epcc_file_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -8,9 +9,17 @@ import (
 
 func TestAccResourceFile(t *testing.T) {
 
+	tempDir := t.TempDir()
 	resource.UnitTest(t, resource.TestCase{
-
 		PreCheck: func() {
+			sourceFile, err := ioutil.ReadFile("../../ep.png")
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = ioutil.WriteFile(tempDir+"/ep.png", sourceFile, 0644)
+			if err != nil {
+				t.Fatal(err)
+			}
 			testAccPreCheck(t)
 		},
 		ProviderFactories: providerFactories,
@@ -18,8 +27,8 @@ func TestAccResourceFile(t *testing.T) {
 			{
 				PreventDiskCleanup: true,
 				Config: `resource "epcc_file" "my_logo"{
-					file_name = "ep.png"
-					file_hash = filemd5("ep.png")
+					file_name = "` + tempDir + `/ep.png"
+					file_hash = filemd5("` + tempDir + `/ep.png")
 					public = true
 				}
 				resource "epcc_file" "my_image_link" {
@@ -27,7 +36,7 @@ func TestAccResourceFile(t *testing.T) {
 				}
 				`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("epcc_file.my_logo", "file_name", "ep.png"),
+					resource.TestCheckResourceAttr("epcc_file.my_logo", "file_name", tempDir+`/ep.png`),
 					resource.TestCheckResourceAttr("epcc_file.my_image_link", "file_location", "https://my.example.com/images/abc.png"),
 				),
 			},
